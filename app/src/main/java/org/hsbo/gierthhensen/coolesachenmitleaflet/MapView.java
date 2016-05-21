29892a8698e7b91e334068a639f02bcbdf8be5ce
lukasgierth <lukas.gierth@mailbox.org>
2016-05-21 17:19:03 +0200

    add some functions.

--- app/src/main/java/org/hsbo/gierthhensen/coolesachenmitleaflet/MapView.java
+++ app/src/main/java/org/hsbo/gierthhensen/coolesachenmitleaflet/MapView.java
@@ -7,6 +7,8 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -16,52 +18,85 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
-@SuppressLint({"SetJavaScriptEnabled"})
+@SuppressLint({"SetJavaScriptEnabled", "ShowToast"})
 public class MapView extends AppCompatActivity implements LocationListener {
 
     public LocationManager locman;
+    public String city = "London";
 
+    /**
+     * Methods
+     *
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_view);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        /**
+         * Show Toast if no internet connection
+         */
+        if (!isOnline()){
+            Toast.makeText(this, "No Internet", Toast.LENGTH_SHORT).show();
+        }
 
         WebView myWebView = (WebView) findViewById(R.id.webview);
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         myWebView.setWebViewClient(new WebViewClient());
         myWebView.loadUrl("file:///android_asset/index.html");
+        myWebView.addJavascriptInterface(new JSInterface(this), "Android");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Search Own Location", Snackbar.LENGTH_SHORT)
-                        .setAction("Action", null).show();
 
-                String lat = "51.8000";
-                String lon = "7.7777";
-                setLocationOnMap(lat, lon);
+                if(city.equals("London")) {
+                    Snackbar.make(view, "Go To Bochum", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+
+                    String lat = "51.4818";
+                    String lon = "7.2162";
+
+                    setLocationOnMap(lat, lon, "Bochum");
+                    city= "Bochum";
+                }
+                else {
+                    Snackbar.make(view, "Back To London", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+
+                    //51.505, -0.09
+                    String lat = "51.505";
+                    String lon = "-0.09";
+
+                    setLocationOnMap(lat, lon, "London");
+                    city = "London";
+                }
 
             }
         });
     }
 
-    public void setLocationOnMap(String lat, String lon) {
+    public void setLocationOnMap(String lat, String lon, String cityName) {
         WebView webView = (WebView) findViewById(R.id.webview);
-        webView.loadUrl("javascript:callFromActivity(" + lat + "," + lon+ ");");
-
+        webView.loadUrl("javascript:toCity(" + lat + "," + lon+ "," + "'" +cityName+ "'" +");");
     }
 
     /**
      * Momentan nicht genutzt, noch fehlerhaft
      */
+
+    /*
     public void getPosition() {
         String lat, lon;
         LocationManager locMan = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
@@ -69,7 +104,7 @@ public class MapView extends AppCompatActivity implements LocationListener {
 
         boolean isNW = locMan.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
 
-        if (isNW == false) {
+        if (!isNW) {
 
         } else {
             Location loc;
@@ -89,6 +124,7 @@ public class MapView extends AppCompatActivity implements LocationListener {
             }
         }
     }
+    */
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -105,16 +141,54 @@ public class MapView extends AppCompatActivity implements LocationListener {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.bplanwms) {
 
             WebView webView = (WebView) findViewById(R.id.webview);
-            webView.loadUrl("javascript:toggleLayer();");
+            webView.loadUrl("javascript:toggleWMS();");
+
+            return true;
+        }
+        if (id == R.id.osmbase) {
+
+            WebView webView = (WebView) findViewById(R.id.webview);
+            webView.loadUrl("javascript:toggleBase();");
 
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+    public boolean isOnline() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnectedOrConnecting()){
+            return true;
+        }
+        else {return false;}
+    }
+
+public class JSInterface {
+    Context context;
+
+    JSInterface(Context c){
+        context = c;
+    }
+
+    @JavascriptInterface
+    public void cityToast() {
+
+        if(city.equals("London")) {
+            Toast.makeText(context, "London", Toast.LENGTH_SHORT).show();
+        }
+        else if (city.equals("Bochum")){
+            Toast.makeText(context, "Bochum", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
+}
 
     @Override
     public void onLocationChanged(Location location){}
